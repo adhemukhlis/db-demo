0da8cd5e16761a85169f018329884cb1acbb6d03
adhemukhlis <adhemukhlis@gmail.com>
2024-01-09 16:36:38 +0700

    init

--- next.config.js
+++ next.config.js
@@ -1,6 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+	publicRuntimeConfig: {
+		// Will be available on both server and client
+		AppName: 'DB Demo'
+	},
+	eslint: {
+		dirs: ['.']
+	},
+	poweredByHeader: false,
+	trailingSlash: false,
+	// transpilePackages: [
+	// 	'antd',
+	// 	'rc-util',
+	// 	'@babel/runtime',
+	// 	'@ant-design/icons',
+	// 	'@ant-design/icons-svg',
+	// 	'rc-pagination',
+	// 	'rc-picker',
+	// 	'rc-tree',
+	// 	'rc-table'
+	// ],
+	reactStrictMode: false
 }
 
 module.exports = nextConfig
